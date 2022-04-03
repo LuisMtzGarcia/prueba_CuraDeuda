935fabca5b0a0b4e824348c84950353ef77f1fdc
Luis <ing.luismtzgarcia@gmail.com>
2022-04-03 18:26:16 -0500

    lectorCSV cleans the estado string and stores it

--- lectorCSV.py
+++ lectorCSV.py
@@ -1,4 +1,5 @@
 import csv
+import unidecode
 
 nombreArchivo = "documentos/csvCombinado.csv"
 """with open(filename, encoding='utf-8') as f:
@@ -25,15 +26,17 @@ header_row = next(lector)
 estados = []
 codigosPostales = []
  
-for row in reader:
-    estado = row[5]
-    cp = int(row[7])
+for fila in lector:
+    estado = fila[5]
+    # Convierte los carácteres con acentos en uno legible por el sistema.
+    estado = unidecode.unidecode(estado)
+    cp = int(fila[7])
     if cp not in codigosPostales:
         codigosPostales.append(cp)
     if estado not in estados:
         estados.append(estado)
 
-file.close()
+archivo.close()
 
 print(estados)
 print(codigosPostales)
